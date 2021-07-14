ec6d7beba37e139feb49c3d85b13bf6a6afddabe
mohamed <muridi51@gmail.com>
2021-07-14 16:09:13 +0100

    drop down added

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import React, { useState } from 'react';
 import episodes from './episodes.json'
 import Episodes from './components/Episodes'
 import Header from './Header'
+
 //import SearchBar from './components/SearchBar';
 
 // console.log(`Imported ${episodes.length} episode(s)`);
 // console.log(`First episode's name is ${episodes[0].name}`);
-
-
-
 
 function App(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState('');
@@ -18,7 +16,20 @@ function App(): JSX.Element {
   val.name.toLowerCase().includes(searchTerm.toLowerCase())||
   val.summary.toLowerCase().includes(searchTerm.toLowerCase()))
 
+  const [dropDown, setDropDown] = useState('')
   
+  const selector = episodes.filter((val)=>
+  val.name === dropDown)
+
+  function allEpisodeDrop(){
+    if (dropDown === ""){
+      return filteredEpisodes
+    }
+    else {
+      return selector
+    }
+  }  
+
 return (
     <>
     <Header/>
@@ -29,9 +40,16 @@ return (
       {setSearchTerm(event.target.value)}} />
       <p>Showing {filteredEpisodes.length}/{episodes.length} episodes</p>
     </div>
-  
+  <label htmlFor = "episode">Choose an episode </label> 
+  <select name= "episode" id= "episode"
+  onChange={(event)=>
+  {setDropDown(event.target.value)}}>
+    <option value= "">...</option>
+    {episodes.map(val=><option key= {val.name}>{val.name}</option>)}
+  </select>
+
   {/* <SearchBar/> */}
-  {filteredEpisodes.map(episode =><Episodes
+  {allEpisodeDrop().map(episode =><Episodes
   key={episode.id}
   name={episode.name}
   season={episode.season}
